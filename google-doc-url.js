299bcab2545d60a44606bb936c68729bfdf47f7c
AhloT <hello.ahrian@gmail.com>
2025-05-30 11:59:34 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_xjrH3gSImzRKqIkhRAqpAAMjH387yaBWfGbidkKtqM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1uYC4MSmAe8VweLelNg3MTLXrpAV3UxzGmTJPt5j1qyk/edit?gid=0#gid=0';
